9a5537f0cde583159a21cb02e88a67778b5625ce
Vihaan0717 <rohitkanna2004@gmail.com>
2026-02-21 21:27:40 +0530

    ignorecodeofline

--- python.py
+++ python.py
@@ -1,3 +1,5 @@
 my_name = "Kanna"
 print("Hello and welcome " + my_name + "!")
 print("What is your name?")
+# This variable will be used to count the number of times anyone tweets the word persnickety
+persnickety_count = 0
